8104863186e05fd2b84e25145d77aacd1f70e131
oriel <orielsilvajr@gmail.com>
2023-12-15 10:12:51 -0500

    Completed

--- algo.js
+++ algo.js
@@ -1,1 +1,41 @@
-// console.log('hello world')
+
+/*
+    Input: a 2 dimensional array of integers
+    Output: A 1 dimensional array of all the same elements preserving original order
+*/
+
+// this given array has a length of 3 because it has 3 arrays as items
+const twoDimArr1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+const expected1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const twoDimArr2 = [[1], [2], [3]];
+const expected2 = [1, 2, 3];
+
+const twoDimArr3 = [[], [], [10, 20]];
+const expected3 = [10, 20];
+
+// --@@--- Fastest way to solve this: console.log(twoDimArr1.flat()) ---@@--
+
+/**
+ * Flattens a two dimensional array into a new one dimensional array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Array<any>>} twoDimArr An array of arrays of any data type.
+ * @returns {Array<any>} The flattened array that should be one dimensional.
+ */
+function flatten2dArray(twoDimArr) {
+    // code here
+    let empty = [];
+    for (let i = 0; i < twoDimArr.length; i++) {
+        for (let j = 0; j < twoDimArr[i].length; j++) {
+            empty.push(twoDimArr[i][j])
+        }
+    };
+    return empty
+};
+
+console.log(flatten2dArray(twoDimArr1))
